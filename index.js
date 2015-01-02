708f33a4abde3caf7af73fcb582780d9c448f1ec
Bjarne Oeverli <bjarne.oeverli@gmail.com>
2015-01-02 20:13:18 +0100

    removed callback function from index. Added vg-parser file instead

--- index.js
+++ index.js
@@ -1,37 +1,10 @@
 'use strict';
 
-var chalk = require('chalk'),
-    argv = require('minimist')(process.argv.slice(2)),
-    request = require('request');
-
-if (argv.h) {
-    console.log('-l limit. Example: vgfeed -l 5.');
-
-    return;
-}
+var request = require('request'),
+    vgParser = require('./lib/vg-parser');
 
 request({
     uri: 'http://www.vg.no/rss/feed/?format=json',
     method: 'GET',
     timeout: 2500
-}, function(err, res, body) {
-    if (err && res.statusCode !== 200) {
-        console.log(err);
-
-        return;
-    }
-
-    JSON.parse(body).some(function(item, key) {
-        if (key !== 0) {
-            console.log('\n');
-        }
-
-        if (argv.l && +argv.l === key) {
-            return true;
-        }
-
-        console.log(chalk.bold(item.title));
-        console.log(chalk.dim(item.published.niceformat + ' - ' + item.persistentUrl));
-        console.log(item.preamble);
-    });
-});
+}, vgParser);
